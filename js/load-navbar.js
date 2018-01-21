32c046124dcd390c0eef6ce0984817caf8883559
sovanny <sovhu774@student.liu.se>
2018-01-21 12:39:06 +0700

    font change

--- js/load-navbar.js
+++ js/load-navbar.js
@@ -1,9 +1,2 @@
 $("#navbar-placeholder").load("navbar.html");
 
-$(document).ready(function(){
-
-        var navHeight = $('nav').outerHeight();
-        $('body').css("padding-top", navHeight);    //makes the content not get hidden under navbar (an effect of loading the navbar with js)
-
-
-});
