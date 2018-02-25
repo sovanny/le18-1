acaadbbe74cd93017b12fb780423fbac748f0b53
sovanny <sovhu774@student.liu.se>
2018-02-25 22:25:22 +0100

    tickets

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 
+$("#fun-btn").on('click', function () {
+    var win = window.open('https://billetto.se/e/location-east-biljetter-260485', '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+});
+
 
 $("#navbar-placeholder").load("navbar.html");
-
 
